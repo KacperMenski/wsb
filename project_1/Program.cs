11370375a4288a20c742ab2fcd54fb9b380e6ff8
KacperMenski <shopen221@gmail.com>
2023-10-06 16:27:30 +0200

    new project

--- project_1/Program.cs
+++ project_1/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace project_1
+﻿using System;
+
+namespace project_1
 {
     internal class Program
-    {
+    { 
+        
+        static double Heron( double x, double y, double z)
+        {
+            double p = (x + y + z) / 2;
+            double w = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+            return w;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Podaj boki trójkąta:");
+            double.TryParse(Console.ReadLine(), out double a);
+            double.TryParse(Console.ReadLine(), out double b);
+            double.TryParse(Console.ReadLine(), out double c);
+            /*if(a+b<=c || a+c<=b || b+c<=a)
+            {
+                Console.WriteLine("nie ma takiego");
+
+            }*/
+            Console.WriteLine("Pole trójkąta:"+Heron(a,b,c));
+
         }
     }
 }
